a18159b2e5046c847463976c17d415a018d6d623
Anthony Davis <no>
2024-08-19 10:31:13 -0400

    modified:   internal/bytecode/bytecode.go

--- internal/bytecode/bytecode.go
+++ internal/bytecode/bytecode.go
@@ -9,7 +9,7 @@ import (
 // ByteCode struct in a separate package to untangle dependencies
 // built by compiler; used by VM
 
-var LangurRev = "0.17.6-dev"
+var LangurRev = "0.18.0-dev"
 
 // to pass from compiler to VM
 type ByteCode struct {
