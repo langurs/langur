7d54ad4ea9bafe662334733049ef406c579b084d
Anthony Davis <no>
2025-12-15 11:46:55 -0500

    working on rev. 0.20.2-dev
    	modified:   DEVELOPMENT.txt
    	modified:   internal/bytecode/bytecode.go

--- internal/bytecode/bytecode.go
+++ internal/bytecode/bytecode.go
@@ -9,7 +9,7 @@ import (
 // ByteCode struct in a separate package to untangle dependencies
 // built by compiler; used by VM
 
-var LangurRev = "0.20.1"
+var LangurRev = "0.20.2-dev"
 
 // to pass from compiler to VM
 type ByteCode struct {
