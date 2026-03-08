53a52230e917bdff55fab5ad4204fa910b17833e
Anthony Davis <no>
2026-03-08 09:57:46 -0400

    release v0.20.3
    	modified:   REVISION_HISTORY.txt
    	modified:   internal/bytecode/bytecode.go

--- internal/bytecode/bytecode.go
+++ internal/bytecode/bytecode.go
@@ -9,7 +9,7 @@ import (
 // ByteCode struct in a separate package to untangle dependencies
 // built by compiler; used by VM
 
-var LangurRev = "0.20.3-dev"
+var LangurRev = "0.20.3"
 
 // to pass from compiler to VM
 type ByteCode struct {
