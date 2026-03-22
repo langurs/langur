54c77cb1353f1ab670b5b9f532d34d0696a3529d
Anthony Davis <no>
2026-03-21 21:19:11 -0400

    release v0.20.4
    	modified:   REVISION_HISTORY.txt
    	modified:   internal/bytecode/bytecode.go
    	modified:   internal/symbol/symbol_table.go

--- internal/symbol/symbol_table.go
+++ internal/symbol/symbol_table.go
@@ -194,14 +194,14 @@ func (st *SymbolTable) resolveSymbol(name string, fromLevel int) (
 	sym, ok = st.store[name]
 
 	if st.DefiningParams {
-		// in a temporarily unresolvable state
+		// symbol table in a temporarily unresolvable state
 		// allows definition of full set of parameters before any can be resolved
-		// (in case any default values include variables of the same name as any parameters)
+		// (in case any default values might otherwise resolve other parameters)
 		ok = false
 	}
 
 	if !ok && st.Outer != nil {
-		// not findable in current symbol table; check outer symbol table
+		// symbol not findable in current symbol table; check outer symbol table
 		// ...but before we increase the level number, verify if defining parameters at this level, ...
 		// ...since opcodes for default parameter values are executed at a different level...
 		// ...(for default values not known at compile-time)
