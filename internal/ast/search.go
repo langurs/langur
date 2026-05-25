78cdbb0e9829f39536135a6533ded2fe96658833
Anthony Davis <no>
2026-05-25 10:53:52 -0400

    changed checking for first scope level declaration to use Node.Search
    	modified:   internal/ast/search.go

--- internal/ast/search.go
+++ internal/ast/search.go
@@ -11,7 +11,6 @@ type searchCriteria struct{
 	DontSearch	[]Node
 }
 
-// a convenience function to list nodes to search
 func (sc *searchCriteria) searchNodes(checkNodes ...Node) (found bool) {
 	return sc.searchNodeSlice(checkNodes)
 }
@@ -33,6 +32,35 @@ func nodeIsOfType(node Node, ofTypes []Node) bool {
 		}
 	}
 	return false
+}
+
+func CopyBeforeAssignment(val Node) bool {
+	// copy value before assignment?
+	// inefficient to always copy
+
+	// FIXME: a temporary patch; inefficient
+	return true
+
+	// return val.Search(copyBeforeAssignment_searchCritera)
+}
+var copyBeforeAssignment_searchCritera = &searchCriteria{
+	OfTypes: []Node{&IdentNode{}},
+	DontSearch: []Node{
+		&LineDeclarationNode{}, &AssignmentNode{}, &ModeNode{},
+		&CallNode{}, &FunctionNode{},
+		&StringNode{}, &RegexNode{}, &DateTimeNode{}, &DurationNode{},
+	},
+}
+
+// for testing whether to wrap into scope
+func NodeContainsFirstScopeLevelDeclaration(node Node) bool {
+	return node.Search(firstScopeLevelDeclaration_searchCriteria)
+}
+var firstScopeLevelDeclaration_searchCriteria = &searchCriteria{
+	OfTypes: []Node{&LineDeclarationNode{}},
+	DontSearch: []Node{
+		&FunctionNode{},
+	},
 }
 
 func IsSimple(node Node) bool {
@@ -61,22 +89,6 @@ func IsSimple(node Node) bool {
 	return false
 }
 
-func CopyBeforeAssignment(val Node) bool {
-	// copy value before assignment?
-	// inefficient to always copy
-
-	// FIXME: a temporary patch; inefficient
-	return true
-
-	// ofTypes := []Node{&IdentNode{}}
-	// dontSearch := []Node{
-	// 	&LineDeclarationNode{}, &ModeNode{}, &CallNode{}, &AssignmentNode{},
-	// 	&StringNode{}, &RegexNode{}, &DateTimeNode{}, &DurationNode{},
-	// }
-
-	// return val.Search(ofTypes, dontSearch)
-}
-
 func EndsWithDefiniteJump(nodes []Node) bool {
 	if len(nodes) > 0 {
 		switch n := nodes[len(nodes)-1].(type) {
@@ -87,192 +99,4 @@ func EndsWithDefiniteJump(nodes []Node) bool {
 		}
 	}
 	return false
-}
-
-// for testing whether to wrap into scope
-func NodeContainsFirstScopeLevelDeclaration(node Node) bool {
-	found, atlevel := NodeSearch(node, 0,
-		[]Node{&LineDeclarationNode{}, &ModeNode{}}, nil)
-
-	if !found || atlevel > 1 {
-		return false
-	}
-	return true
-}
-
-func NodeSearch(node Node, level int, ofTypes, stopAt []Node) (found bool, atlevel int) {
-	if nodeIsOfType(node, ofTypes) {
-		return true, level
-	}
-	if nodeIsOfType(node, stopAt) {
-		return false, level
-	}
-
-	switch n := node.(type) {
-	// not going into a function node here...
-
-	case *Program:
-		for _, v := range n.Statements {
-			if found, atlevel = NodeSearch(v, level, ofTypes, stopAt); found {
-				return
-			}
-		}
-
-	case *LineDeclarationNode:
-		return NodeSearch(n.Assignment, level+1, ofTypes, stopAt)
-
-	case *AssignmentNode:
-		for _, v := range n.Values {
-			if found, atlevel = NodeSearch(v, level, ofTypes, stopAt); found {
-				return
-			}
-		}
-
-	case *ModeNode:
-		return NodeSearch(n.Setting, level, ofTypes, stopAt)
-
-	case *CallNode:
-		for _, arg := range n.PositionalArgs {
-			if found, atlevel = NodeSearch(arg, level, ofTypes, stopAt); found {
-				return
-			}
-		}
-		for _, arg := range n.ByNameArgs {
-			if found, atlevel = NodeSearch(arg, level, ofTypes, stopAt); found {
-				return
-			}
-		}
-
-	case *StringNode:
-		for _, v := range n.Interpolations {
-			if found, atlevel = NodeSearch(v, level, ofTypes, stopAt); found {
-				return
-			}
-		}
-
-	case *InterpolatedNode:
-		found, atlevel = NodeSearch(n.Value, level, ofTypes, stopAt)
-	case *RegexNode:
-		found, atlevel = NodeSearch(n.Pattern, level, ofTypes, stopAt)
-	case *DateTimeNode:
-		found, atlevel = NodeSearch(n.Pattern, level, ofTypes, stopAt)
-	case *DurationNode:
-		found, atlevel = NodeSearch(n.Pattern, level, ofTypes, stopAt)
-
-	case *ListNode:
-		for _, e := range n.Elements {
-			if found, atlevel = NodeSearch(e, level, ofTypes, stopAt); found {
-				return
-			}
-		}
-
-	case *HashNode:
-		for _, kv := range n.Pairs {
-			if found, atlevel = NodeSearch(kv.Key, level, ofTypes, stopAt); found {
-				return
-			}
-			if found, atlevel = NodeSearch(kv.Value, level, ofTypes, stopAt); found {
-				return
-			}
-		}
-
-	case *IndexNode:
-		if found, atlevel = NodeSearch(n.Left, level, ofTypes, stopAt); found {
-			return
-		}
-		if found, atlevel = NodeSearch(n.Index, level, ofTypes, stopAt); found {
-			return
-		}
-		found, atlevel = NodeSearch(n.Alternate, level, ofTypes, stopAt)
-
-	case *BlockNode:
-		add := 0
-		if n.HasScope {
-			add = 1
-		}
-		for _, stmt := range n.Statements {
-			if found, atlevel = NodeSearch(stmt, level+add, ofTypes, stopAt); found {
-				return
-			}
-		}
-
-	case *ExpressionStatementNode:
-		found, atlevel = NodeSearch(n.Expression, level, ofTypes, stopAt)
-
-	case *InfixExpressionNode:
-		if found, atlevel = NodeSearch(n.Left, level, ofTypes, stopAt); found {
-			return
-		}
-		found, atlevel = NodeSearch(n.Right, level, ofTypes, stopAt)
-
-	case *PrefixExpressionNode:
-		found, atlevel = NodeSearch(n.Right, level, ofTypes, stopAt)
-
-	case *PostfixExpressionNode:
-		found, atlevel = NodeSearch(n.Left, level, ofTypes, stopAt)
-
-	case *ForNode:
-		found, atlevel = NodeSearch(n.Body, level+1, ofTypes, stopAt)
-
-	case *ForInOfNode:
-		found, atlevel = NodeSearch(n.Body, level+1, ofTypes, stopAt)
-
-	case *IfNode:
-		for _, ta := range n.TestsAndActions {
-			if found, atlevel = NodeSearch(ta.Test, level+1, ofTypes, stopAt); found {
-				return true, level
-			}
-			if found, atlevel = NodeSearch(ta.Do, level+1, ofTypes, stopAt); found {
-				return true, level
-			}
-		}
-
-	case *SwitchNode:
-		for _, e := range n.Expressions {
-			if found, atlevel = NodeSearch(e.Expr, level+1, ofTypes, stopAt); found {
-				return true, level
-			}
-		}
-		for _, ca := range n.CasesAndActions {
-			if found, atlevel = NodeSearch(ca.Do, level+1, ofTypes, stopAt); found {
-				return true, level
-			}
-			for _, cond := range ca.MatchConditions {
-				if found, atlevel = NodeSearch(cond, level+1, ofTypes, stopAt); found {
-					return true, level
-				}
-			}
-			for _, cond := range ca.OtherConditions {
-				if found, atlevel = NodeSearch(cond, level+1, ofTypes, stopAt); found {
-					return true, level
-				}
-			}
-		}
-
-	case *TryCatchNode:
-		if found, atlevel = NodeSearch(n.Try, level, ofTypes, stopAt); found {
-			return
-		}
-		if found, atlevel = NodeSearch(n.Catch, level+1, ofTypes, stopAt); found {
-			return
-		}
-		found, atlevel = NodeSearch(n.Else, level+1, ofTypes, stopAt)
-
-	case *ThrowNode:
-		found, atlevel = NodeSearch(n.Exception, level, ofTypes, stopAt)
-
-	case *ReturnNode:
-		found, atlevel = NodeSearch(n.ReturnValue, level, ofTypes, stopAt)
-
-	case *ExpansionNode:
-		if found, atlevel = NodeSearch(n.Limits, level, ofTypes, stopAt); found {
-			return
-		}
-		found, atlevel = NodeSearch(n.Continuation, level, ofTypes, stopAt)
-		
-	default:
-		// a bug
-	}
-
-	return
 }
