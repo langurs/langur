400e6f8cee1565fdc02854af1c0dade083130c29
Anthony Davis <no>
2026-06-17 18:10:41 -0400

    added ReadBytes() and ReadCodePoints() internal console IO functions to read a specified number of bytes or code points
    	modified:   internal/text_io/console.go

--- internal/text_io/console.go
+++ internal/text_io/console.go
@@ -3,10 +3,12 @@
 package text_io
 
 import (
+	"io"
 	"bufio"
 	"fmt"
 	"langur/str"
 	"os"
+	"unicode/utf8"
 )
 
 func PrintErr(s string, replaceNewLines bool) {
@@ -39,5 +41,58 @@ func ReadLn(replaceNewLines bool) (s string, scanned bool) {
 	if replaceNewLines {
 		s = str.ReplaceNewLinesWithLinux(s)
 	}
+	return
+}
+
+func ReadBytes(byteCount int, replaceNewLines bool) (s string, scanned bool) {
+	buf := make([]byte, byteCount)
+	_, err := io.ReadFull(os.Stdin, buf)
+	scanned = err == nil
+	s = string(buf)
+	if replaceNewLines {
+		s = str.ReplaceNewLinesWithLinux(s)
+	}
+	return
+}
+
+func ReadCodePoints(cpCount int, replaceNewLines bool) (s string, err error) {
+	// Last we knew, the Unicode standard wouldn't have more than 4 bytes in a UTF-8 sequence.
+	byteMax := 4
+
+	r := bufio.NewReader(os.Stdin)
+
+	var out []byte
+	var cp []byte
+	var b byte
+
+	count := 0
+	for count < cpCount {
+		// get 1 byte
+		b, err = r.ReadByte()
+		if err != nil {
+			break
+		}
+
+		// append to code point slice
+		cp = append(cp, b)
+		if utf8.FullRune(cp) {
+			// valid code code point; append to total slice and count
+			out = append(out, cp...)
+			cp = nil
+			count++
+		}
+
+		if len(cp) > byteMax {
+			// b/c of potential errors in input, error out here
+			err = fmt.Errorf("Too many bytes in UTF-8 sequence that do not create a single code point")
+			break
+		}
+	}
+
+	s = string(out)
+	if replaceNewLines {
+		s = str.ReplaceNewLinesWithLinux(s)
+	}
+
 	return
 }
